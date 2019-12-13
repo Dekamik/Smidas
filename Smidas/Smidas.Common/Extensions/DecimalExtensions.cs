f8f1205936e5ede3b9fe4b4ebde4f1dbd525c418
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2019-12-13 15:18:35 +0100

    Obliterated Selenium

--- Smidas/Smidas.Common/Extensions/DecimalExtensions.cs
+++ Smidas/Smidas.Common/Extensions/DecimalExtensions.cs
@@ -6,9 +6,10 @@ namespace Smidas.Common.Extensions
 {
     public static class DecimalExtensions
     {
-        public static decimal NextDecimal(this Random rand)
-        {
-            return new decimal(rand.NextDouble());
-        }
+        public static decimal NextDecimal(this Random rand) => new decimal(rand.NextDouble());
+
+        public static decimal ParseDecimal(this string str) => decimal.Parse(!string.IsNullOrEmpty(str) ? str : "0");
+
+        public static decimal ParseDecimalWithSymbol(this string str) => decimal.Parse(str[0..^1] ?? "0");
     }
 }
---
+++ Smidas/Smidas.WebScraping/Extensions/DecimalExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smidas.WebScraping.Extensions
+{
+    public static class DecimalExtensions
+    {
+        
+    }
+}
